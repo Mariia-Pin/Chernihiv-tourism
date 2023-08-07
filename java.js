6240a93b2d25efe22dde5d1ab24cbfdb02b9fe36
Mariia-Pin <pesnja07@ukr.net>
2023-08-07 13:47:00 +0300

    update 0708

--- java.js
+++
@@ -1,8 +0,0 @@
-// JavaScript для переключення мобільної навігації
-function toggleMenu() {
-    var navbarMenu = document.querySelector('.menu');
-    var navbarToggle = document.querySelector('.navbar-toggle');
-
-    navbarToggle.classList.toggle('active');
-    navbarMenu.classList.toggle('active');
-}
